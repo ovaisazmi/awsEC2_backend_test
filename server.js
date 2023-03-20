d56407b4120252e405cad66ac81374f2c89ed2a1
owais azmi <owaisazmi@MacBook-Air.local>
2023-03-20 09:32:05 +0530

    final

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const express=require("express");
-const http=require("http");
-const socketio=require("socket.io");
-const cricLive = require('cric-live');
-
 const app=express();
-const server=http.createServer(app);
 
 app.get("/",(req,res)=>{
     console.log("server working fine");
@@ -12,14 +7,11 @@ app.get("/",(req,res)=>{
 })
 
 
-server.listen(8000,()=>{
+app.listen(8000,()=>{
     console.log("server is running at 8000");
 })
 
-const io=socketio(server);
 
-io.on("connection",(socket)=>{
-    console.log("New Client Joined")
-})
+
 
 
